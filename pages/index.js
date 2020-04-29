0f633f68a6c505595b420abcfae6d59d174023ba
Edward Parrish <elp49@drexel.edu>
2020-04-29 09:42:45 -0400

    Index Announcements - added announcements list to Home page

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,45 @@ const index = {
     { imageUrl: '/images/clothing-14.jpg', title: 'Always accepting clothing donations', buttonText: 'Learn more', linkTo: '/outreach' },
     { imageUrl: '/images/kensington-1.jpg', title: 'Find out where your donations go', buttonText: 'Learn more', linkTo: '/outreach' },
     { imageUrl: '/images/kensington-5.jpg', title: 'Help those with the greatest needs', buttonText: 'Learn more', linkTo: '/outreach' }
-  ]
+  ],
+  announcements: {
+    title: 'Announcements',
+    subTitle: 'Sign up for our mailing list to stay up to date on our latest announcements.',
+    posts: [
+      { header: 'We have some news', details: 'Something happend and now we would like to tell you about it. Read this to find out what it is we want to tell you. If something else happens we will also let you know about that.' },
+      { header: 'This is another announcement', details: 'Something happend and now we would like to tell you about it. Read this to find out what it is we want to tell you. If something else happens we will also let you know about that.' },
+      { header: 'We have something to announce', details: 'Something happend and now we would like to tell you about it. Read this to find out what it is we want to tell you. If something else happens we will also let you know about that.' },
+      { header: 'Read this to know what happened', details: 'Something happend and now we would like to tell you about it. Read this to find out what it is we want to tell you. If something else happens we will also let you know about that.' },
+    ]
+  }
 };
 
 export default function Index() {
   return (
     <Layout>
-      <div className={styles.container}>
-        <div className={styles.slideshowContainer}>
+      <div id={'index'}>
+        <div className={styles.indexSlideshow}>
           <Slideshow slides={index.slides} />
+        </div>
+        <div className={styles.indexAnnouncements}>
+          <div className={styles.announcements}>
+            <div className={styles.announcementsTitle}>
+              <h1>{index.announcements.title}</h1>
+              <h3>{index.announcements.subTitle}</h3>
+            </div>
+            <ul>
+              {index.announcements.posts.map((post, i) => {
+                return (
+                  <li key={`post-${i}`} className={styles.post}>
+                    <div className={styles.postHeader}><h1>{post.header}</h1></div>
+                    <div className={styles.postDetails}>
+                      <p>{post.details}</p>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </Layout>

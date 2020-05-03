346ebc3291569c5c4243c79b3893e7e09dd8c111
Edward Parrish <elp49@drexel.edu>
2020-05-03 15:36:58 -0400

    Layout Overlay - fixing scroll bar inconsistency in header on Overlay on web browser screens less than 1000px

--- components/Layout.js
+++ components/Layout.js
@@ -14,37 +14,42 @@ const layout = {
 
 export const toggleOverlay = () => {
   document.getElementById('layout').classList.toggle(styles.layoutFixed);
-  document.getElementById('overlay').classList.toggle(styles.overlayActive);
+  document.getElementById('layoutOverlay').classList.toggle(styles.overlayActive);
+  document.getElementById('container').classList.toggle(styles.container);
 };
 
 export default function Layout(props) {
   return (
-    <div id={'layout'}>
-      <div id={'overlay'} className={styles.layoutOverlay}>
-        <Overlay pages={layout.pages} />
-      </div>
-      <div className={styles.layoutHeader}>
-        <div className={styles.header}>
-          <div className={styles.hamburger} onClick={toggleOverlay}>
-            <span></span>
-            <span></span>
-            <span></span>
+    <div id={'layout'} className={styles.layout}>
+      <div id={'container'}>
+        <div className={styles.layoutHeader}>
+          <div className={styles.header}>
+            <div className={styles.hamburger} onClick={toggleOverlay}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <a href='/' className={styles.logo} title={'Open Door'}>
+              <img alt={'Open Door'} src={layout.logo} style={{ height: 100 + '%' }} />
+            </a>
+            <ul className={styles.headerPages}>
+              {layout.pages.map((page, i) => {
+                return (
+                  <li key={`page-${i}`} className={styles.page}>
+                    <a href={`/${page._id}`} title={page.name}>{page.name}</a>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
-          <a href='/' className={styles.logo} title={'Open Door'}>
-            <img alt={'Open Door'} src={layout.logo} style={{ height: 100 + '%' }} />
-          </a>
-          <ul className={styles.headerPages}>
-            {layout.pages.map((page, i) => {
-              return (
-                <li key={`page-${i}`} className={styles.page}>
-                  <a href={`/${page._id}`} title={page.name}>{page.name}</a>
-                </li>
-              )
-            })}
-          </ul>
+        </div>
+        <div id={'layoutOverlay'} className={styles.layoutOverlay}>
+          <Overlay pages={layout.pages} />
         </div>
       </div>
-      {props.children}
+      <div>
+        {props.children}
+      </div>
     </div>
   )
 }

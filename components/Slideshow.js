1afb573c250ec9fb13e1005c1eb58bcde0dea8b4
Edward Parrish <elp49@drexel.edu>
2020-05-15 14:34:26 -0400

    Slideshow - removing global timeout variable from Slideshow component, changing slide change effect to fading

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -1,59 +1,64 @@
 import styles from './Slideshow.module.css'
 
-var timeout;
-var currentSlide;
+function checkTimeout(id, timePassed) {
+  const slideshow = document.getElementById(`${id}Slideshow`);
+  let timeRem = parseInt(slideshow.getAttribute('time-rem'));
+  timeRem -= timePassed;
+  if (timeRem > 0) {
+    slideshow.setAttribute('time-rem', timeRem);
+  } else {
+    const nextSlideI = parseInt(document.getElementById(`${id}Slideshow`).getAttribute('current-slide-i')) + 1;
+    showSlide(id, nextSlideI);
+  }
+  setTimeout(() => { checkTimeout(id, timePassed) }, timePassed);
+}
+
 function showSlide(id, n) {
-  if (typeof n === 'undefined') {
-    return;
-  }
-  let nextSlide = n;
-  let i;
-  let slides;
-  let badges;
-  if (nextSlide !== currentSlide) {
-    slides = document.getElementById(`${id}Slideshow`).getElementsByClassName(styles.slide);
-    badges = document.getElementById(`${id}BadgeContainer`).getElementsByClassName(styles.badge);
-    if (slides.length < 1 || badges.length < 1) {
-      return;
-    }
-    for (i = 0; i < slides.length; i++) {
-      if (slides[i].classList.contains(styles.slideActive)) {
-        slides[i].classList.remove(styles.slideActive);
-      }
-    }
-    for (i = 0; i < badges.length; i++) {
-      if (badges[i].classList.contains(styles.badgeActive)) {
-        badges[i].classList.remove(styles.badgeActive);
-      }
-    }
-    if (nextSlide >= slides.length || nextSlide < 0) {
-      nextSlide = 0;
-    }
-    slides[nextSlide].classList.add(styles.slideActive);
-    badges[nextSlide].classList.add(styles.badgeActive);
-    currentSlide = nextSlide;
-  }
-  clearTimeout(timeout);
-  timeout = setTimeout(() => { showSlide(id, currentSlide + 1) }, 5000);
+  const slideshow = document.getElementById(`${id}Slideshow`);
+  const currentSlideI = parseInt(slideshow.getAttribute('current-slide-i'));
+  const nextSlideI = getNextSlideIndex(id, n);
+  document.getElementById(`${id}Slide-${currentSlideI}`).style.opacity = 0;
+  document.getElementById(`${id}Slide-${nextSlideI}`).style.opacity = 1;
+  document.getElementById(`${id}Badge-${currentSlideI}`).getElementsByTagName('span')[0].style.opacity = 0.3;
+  document.getElementById(`${id}Badge-${nextSlideI}`).getElementsByTagName('span')[0].style.opacity = 0.9;
+  slideshow.setAttribute('current-slide-i', nextSlideI);
+  const slideshowTime = parseInt(slideshow.getAttribute('timeout'));
+  slideshow.setAttribute('time-rem', slideshowTime);
 };
+
+function getNextSlideIndex(id, n) {
+  const lastSlideI = document.getElementById(`${id}Slideshow`).getElementsByClassName(styles.slide).length - 1;
+  let nextIdx;
+  if (typeof n === 'undefined' || n > lastSlideI) {
+    nextIdx = 0;
+  } else if (n < 0) {
+    nextIdx = lastSlideI;
+  } else {
+    nextIdx = n;
+  }
+  return nextIdx;
+}
 
 export async function componentDidMount(id) {
   if (typeof window === 'undefined') {
     return;
   }
+  const timeoutTime = 1000;
+  setTimeout(() => { checkTimeout(id, timeoutTime) }, timeoutTime);
   showSlide(id, 0);
 }
 
 export default function Slideshow(props) {
   const id = props.slideshow._id;
   const slides = props.slideshow.slides.sort((a, b) => { a._id - b._id });
+  const slideshowTime = parseInt(props.slideshow.timeout) * 1000;
   componentDidMount(id);
   return (
-    <div className={props.slideshow.theme} style={{ height: 100 + '%', position: 'relative' }}>
-      <ul id={`${id}Slideshow`} className={styles.slideshow}>
+    <div id={`${id}Slideshow`} className={styles.slideshow} current-slide-i={0} timeout={slideshowTime} time-rem={slideshowTime}>
+      <ul className={styles.slideList}>
         {slides.map((slide, i) => {
           return (
-            <li key={`${id}Slide-${i}`} className={styles.slide} style={{ backgroundImage: `url(${slide.url})` }}>
+            <li key={`${id}Slide-${i}`} id={`${id}Slide-${i}`} className={styles.slide} style={{ backgroundImage: `url(${slide.url})` }}>
               <div className={styles.slideHeader}>
                 <h1>{slide.title}</h1>
               </div>
@@ -64,25 +69,15 @@ export default function Slideshow(props) {
           )
         })}
       </ul>
-      <ul id={`${id}BadgeContainer`} className={styles.badgesContainer}>
+      <ul className={styles.badgeList}>
         {slides.map((slide, i) => {
           return (
-            <li key={`${id}Badge-${i}`} className={styles.badge} onClick={() => showSlide(id, i)}>
+            <li key={`${id}Badge-${i}`} id={`${id}Badge-${i}`} className={styles.badge} onClick={() => { showSlide(id, i); }}>
               <span></span>
             </li>
           )
         })}
       </ul>
-      <style jsx>{`
-      .white {
-        background-color: #fff;
-        color: #000;
-      }
-      .blue {
-        background-color: #24316F;
-        color: #fff;
-      }
-      `}</style>
     </div>
   )
 }

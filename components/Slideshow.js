f04f2ff33b9f64b4705b7ef368b26c2e2a480ae0
elp49 <elp49@drexel.edu>
2020-06-09 22:29:47 -0400

    Refactored Slideshow component

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -1,122 +1,236 @@
 import styles from './Slideshow.module.css'
 
-function checkTimeout(id, timePassed) {
-  const slideshow = document.getElementById(`${id}Slideshow`);
-  let timeRem = parseInt(slideshow.getAttribute('time-rem'));
-  timeRem -= timePassed;
-  if (timeRem > 0) {
-    slideshow.setAttribute('time-rem', timeRem);
-  } else {
-    showSlide(id);
-  }
-  setTimeout(() => checkTimeout(id, timePassed), timePassed);
+const NO_SLIDES = -1;
+const MILLISECONDS_BEFORE_NEXT_CHECK_TIMEOUT = 1000;
+
+function getSlideshowElementById(slideshowId) {
+  return document.getElementById(`${slideshowId}Slideshow`);
 }
 
-function showSlide(id, i) {
-  const slideshow = document.getElementById(`${id}Slideshow`);
-  const currentSlideI = parseInt(slideshow.getAttribute('current-slide-i'));
-  const nextSlideI = typeof i === 'undefined' ? getNextSlideIndex(id) : i;
-  if (nextSlideI === null) {
-    return;
-  }
-  document.getElementById(`${id}Slide-${currentSlideI}`).style.opacity = 0;
-  document.getElementById(`${id}Slide-${nextSlideI}`).style.opacity = 1;
-  document.getElementById(`${id}Badge-${currentSlideI}`).getElementsByTagName('span')[0].style.opacity = 0.3;
-  document.getElementById(`${id}Badge-${nextSlideI}`).getElementsByTagName('span')[0].style.opacity = 0.9;
-  slideshow.setAttribute('current-slide-i', nextSlideI);
-  const slideshowTime = parseInt(slideshow.getAttribute('timeout'));
-  slideshow.setAttribute('time-rem', slideshowTime);
-};
+function getSlideshowTimeoutDuration(slideshowId) {
+  const slideshow = getSlideshowElementById(slideshowId)
+  return parseInt(slideshow.getAttribute('timeout-duration'));
+}
 
-function getNextSlideIndex(id) {
-  const slideshow = document.getElementById(`${id}Slideshow`);
-  const selector = window.innerWidth < 500 ? `.${styles.slideBoth}, .${styles.slideMobileOnly}` : `.${styles.slideBoth}, .${styles.slideDesktopOnly}`;
-  const slides = slideshow.querySelectorAll(selector);
-  if (slides.length === 0) {
-    return null;
-  }
-  const currentSlideI = parseInt(slideshow.getAttribute('current-slide-i'));
+function getSlideshowTimeoutTimeRemaining(slideshowId) {
+  const slideshow = getSlideshowElementById(slideshowId)
+  return parseInt(slideshow.getAttribute('time-remaining'));
+}
+
+function setSlideshowTimeoutTimeRemaining(slideshowId, timeRemaining) {
+  const slideshow = getSlideshowElementById(slideshowId)
+  slideshow.setAttribute('time-remaining', timeRemaining);
+}
+
+function resetSlideshowTimeoutTimeRemaining(slideshowId) {
+  const timeoutDuration = getSlideshowTimeoutDuration(slideshowId)
+  setSlideshowTimeoutTimeRemaining(slideshowId, timeoutDuration);
+}
+
+function getSlideElementsOnMobileScreenWidth(slideshowId) {
+  const slideshow = getSlideshowElementById(slideshowId);
+  return slideshow.querySelectorAll(`.${styles.slide} .${styles.showOnMobileOnly}`);
+}
+
+function getSlideElementsOnDesktopScreenWidth(slideshowId) {
+  const slideshow = getSlideshowElementById(slideshowId);
+  return slideshow.querySelectorAll(`.${styles.slide} .${styles.showOnDesktopOnly}`);
+}
+
+function isMobileScreen() {
+  if (window.innerWidth < 500) return true;
+  return false;
+}
+
+function getCurrentlyVisibleSlideElements(slideshowId) {
+  if (isMobileScreen()) return getSlideElementsOnMobileScreenWidth(slideshowId);
+  return getSlideElementsOnDesktopScreenWidth(slideshowId);
+}
+
+function getThisCurrentSlidesIndex(slide) {
+  return parseInt(slide.getAttribute('value'));
+}
+
+function getCurrentSlideIndex(slideshowId) {
+  const slideshow = getSlideshowElementById(slideshowId)
+  return parseInt(slideshow.getAttribute('current-slide-index'));
+}
+
+function setCurrentSlideIndex(slideshowId, currentSlideIndex) {
+  const slideshow = getSlideshowElementById(slideshowId)
+  slideshow.setAttribute('current-slide-index', currentSlideIndex);
+}
+
+function getNextSlideIndex(slideshowId) {
+  let slides = getCurrentlyVisibleSlideElements(slideshowId);
+  if (slides.length === 0) return NO_SLIDES;
+
+  const currentSlideIndex = getCurrentSlideIndex(slideshowId);
   for (let i = 0; i < slides.length; i++) {
-    const slideI = parseInt(slides[i].getAttribute('value'));
-    if (slideI > currentSlideI) {
-      return slideI;
-    }
+
+    const slideIndex = getThisCurrentSlidesIndex(slides[i]);
+    if (slideIndex > currentSlideIndex) return slideIndex;
+
   }
+
   return parseInt(slides[0].getAttribute('value'));
 }
 
-function handleResize(id, screenWidth) {
+function setSlideOpacity(slideshowId, currentSlideIndex, nextSlideIndex) {
+  document.getElementById(`${slideshowId}Slide-${currentSlideIndex}`).style.opacity = 0;
+  document.getElementById(`${slideshowId}Slide-${nextSlideIndex}`).style.opacity = 1;
+}
+
+function setBadgeOpacity(slideshowId, currentSlideIndex, nextSlideIndex) {
+  document.getElementById(`${slideshowId}Badge-${currentSlideIndex}`).getElementsByTagName('span')[0].style.opacity = 0.3;
+  document.getElementById(`${slideshowId}Badge-${nextSlideIndex}`).getElementsByTagName('span')[0].style.opacity = 0.9;
+}
+
+function showSlide(slideshowId, slideIndex) {
+  const currentSlideIndex = getCurrentSlideIndex(slideshowId);
+  const nextSlideIndex = typeof slideIndex !== 'undefined' ? slideIndex : getNextSlideIndex(slideshowId);
+
+  if (nextSlideIndex === NO_SLIDES) return;
+
+  setSlideOpacity(slideshowId, currentSlideIndex, nextSlideIndex);
+  setBadgeOpacity(slideshowId, currentSlideIndex, nextSlideIndex);
+
+  setCurrentSlideIndex(slideshowId, nextSlideIndex);
+  resetSlideshowTimeoutTimeRemaining(slideshowId);
+};
+
+function checkTimeout(slideshowId, timePassed) {
+  let timeRemaining = getSlideshowTimeoutTimeRemaining(slideshowId);
+
+  timeRemaining -= timePassed;
+  if (timeRemaining > 0) setSlideshowTimeoutTimeRemaining(slideshowId, timeRemaining);
+  else showSlide(slideshowId);
+  
+  setTimeout(() => checkTimeout(slideshowId, timePassed), timePassed);
+}
+
+function screenResizePassesMobileDesktopBarrier(screenWidth, newWidth) {
+  if ((screenWidth >= 500 && newWidth < 500) || (screenWidth < 500 && newWidth >= 500)) return true;
+  return false;
+}
+
+function handleResize(slideshowId, screenWidth) {
   const newWidth = window.innerWidth;
-  if ((screenWidth >= 500 && newWidth < 500) || (screenWidth < 500 && newWidth >= 500)) {
-    showSlide(id);
-  }
+  if (screenResizePassesMobileDesktopBarrier(screenWidth, newWidth)) showSlide(slideshowId);
+
   return newWidth;
 }
 
-export async function componentDidMount(id) {
-  if (typeof window === 'undefined') {
-    return;
-  }
-  const timeoutTime = 1000;
-  setTimeout(() => checkTimeout(id, timeoutTime), timeoutTime);
-  showSlide(id, 0);
+export async function componentDidMount(slideshowId, numSlides) {
+  if (typeof window === 'undefined') return;
   let screenWidth = window.innerWidth;
-  window.addEventListener('resize', () => screenWidth = handleResize(id, screenWidth));
+
+  if (numSlides > 1) setTimeout(() => checkTimeout(slideshowId, MILLISECONDS_BEFORE_NEXT_CHECK_TIMEOUT), MILLISECONDS_BEFORE_NEXT_CHECK_TIMEOUT);
+
+  showSlide(slideshowId, 0);
+
+  window.addEventListener('resize', () => {
+    screenWidth = handleResize(slideshowId, screenWidth);
+  });
 }
 
-export default function Slideshow(props) {
-  if (typeof props.slideshow === 'undefined' || typeof props.slideshow.name === 'undefined') {
-    return null;
+function slideIsActive(showOnMobile, showOnDesktop) {
+  if (showOnMobile || showOnDesktop) return true;
+  return false;
+}
+
+function getSlideClassName(showOnMobile, showOnDesktop) {
+  if (!showOnDesktop) return styles.showOnMobileOnly;
+  else if (!showOnMobile) return styles.showOnDesktopOnly;
+  else return '';
+}
+
+function slideListReducerCallback(list, slide) {
+  if (slideIsActive(slide.showOnMobile, slide.showOnDesktop)) {
+
+    const className = getSlideClassName(slide.showOnMobile, slide.showOnDesktop);
+    list.push({
+      className: className,
+      image: slide.image.replace(/\\/g, '/'),
+      titles: slide.titles,
+      buttons: slide.buttons
+    });
+
   }
-  const id = props.slideshow.name;
-  var slides = props.slideshow.slides.sort((a, b) => a.order - b.order);
-  slides.forEach(slide => {
-    if (slide.showOnMobile || slide.showOnDesktop) {
-      slides.image = slide.image.replace('\\', '/');
-    }
-  });
-  const slideshowTime = parseInt(props.slideshow.timeout) * 1000;
-  componentDidMount(id);
+
+  return list;
+}
+
+function reduceSlideList(slideList) {
+  return slideList.sort((a, b) => b.reverseOrder - a.reverseOrder)
+    .reduce((list, slide) => slideListReducerCallback(list, slide), []);
+}
+
+export default function Slideshow({ slideshow }) {
+  if (typeof slideshow === 'undefined' || slideshow.slideList.length === 0) return;
+
+  const id = typeof slideshow.name !== 'undefined' ? slideshow.name : 'slideshow';
+  const timeoutDuration = typeof slideshow.timeoutDuration === 'undefined' ? 7000 : parseInt(slideshow.timeoutDuration) * 1000;
+  const slideList = reduceSlideList(slideshow.slideList);
+
+  componentDidMount(id, slideList.length);
+
   return (
-    <div id={`${id}Slideshow`} className={styles.slideshow} current-slide-i={0} timeout={slideshowTime} time-rem={slideshowTime}>
+    <div id={`${id}Slideshow`} className={styles.slideshow} current-slide-index={0} timeout-duration={timeoutDuration} time-remaining={timeoutDuration}>
       <ul className={styles.slideList}>
-        {slides.map((slide, i) => {
-          let className;
-          if (!slide.showOnDesktop) {
-            className = styles.slideMobileOnly;
-          } else if (!slide.showOnMobile) {
-            className = styles.slideDesktopOnly;
-          } else {
-            className = styles.slideBoth;
-          }
-          return (
-            <li key={`${id}Slide-${i}`} id={`${id}Slide-${i}`} value={i} className={className} style={{ backgroundImage: `url(${process.env.OPEN_DOOR_API}${slide.image})` }}>
-              <div className={styles.slideHeader}>
-                <h1>{slide.title}</h1>
-              </div>
-              <a href={slide.href} className={styles.slideButton}>
-                <h3>{slide.buttonText}</h3>
-              </a>
-            </li>
-          )
-        })}
+        {
+          slideList.map((slide, i) => {
+            const key = `${id}Slide-${i}`;
+            const { className, image, titles, buttons } = slide;
+
+            return (
+              // <li key={`${id}Slide-${i}`} id={`${id}Slide-${i}`} className={className} style={{ backgroundImage: `url(${process.env.OPEN_DOOR_API}${slide.image})` }}>
+              <li key={key} id={key} className={`${styles.slide} ${className}`} style={{ backgroundImage: `url(${image})` }}>
+                {
+                  titles.map(title => {
+                    const titleKey = `${id}SlideTitle-${i}`;
+
+                    return (
+                      <div key={titleKey} className={styles.slideHeader}>
+                        <h1>{title}</h1>
+                      </div>
+                    );
+                  })
+                }
+                <ul className={styles.buttonList}>
+                  {
+                    buttons.map(button => {
+                      const buttonKey = `${id}SlideButton-${i}`;
+                      const { href, text } = button;
+
+                      return (
+                        <li key={buttonKey} className={styles.slideButton}>
+                          <a href={href} className={styles.button}>
+                            <h3>{text}</h3>
+                          </a>
+                        </li>
+                      );
+                    })
+                  }
+                </ul>
+              </li>
+            )
+          })
+        }
       </ul>
       <ul className={styles.badgeList}>
-        {slides.map((slide, i) => {
-          let className;
-          if (!slide.showOnDesktop) {
-            className = styles.badgeMobileOnly;
-          } else if (!slide.showOnMobile) {
-            className = styles.badgeDesktopOnly;
-          } else {
-            className = styles.badgeBoth;
-          }
-          return (
-            <li key={`${id}Badge-${i}`} id={`${id}Badge-${i}`} className={className} onClick={() => showSlide(id, i)}>
-              <span></span>
-            </li>
-          )
-        })}
+        {
+          slideList.map((slide, i) => {
+            const key = `${id}Badge-${i}`;
+            const { className } = slide;
+
+            return (
+              <li key={key} id={key} className={`${styles.badge} ${className}`} onClick={() => showSlide(id, i)}>
+                <span></span>
+              </li>
+            )
+          })
+        }
       </ul>
     </div>
   )

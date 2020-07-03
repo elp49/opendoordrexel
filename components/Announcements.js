54b3cba392f85d433b4d9d04413014b81898e5b0
elp49 <elp49@drexel.edu>
2020-07-03 14:21:20 -0400

    Announcements - refactoring and automating post jsx creation

--- components/Announcements.js
+++ components/Announcements.js
@@ -7,58 +7,102 @@ function isDefined(a) {
   return false;
 }
 
-function sortListByOrder(list) {
-  return list.sort((a, b) => a.order - b.order);
+function sortListByReverseOrder(list) {
+  return list.sort((a, b) => b.reverseOrder - a.reverseOrder);
+}
+
+function postIsValid(post) {
+  const { header, details } = post;
+  if (isDefined(header) && header !== ''
+    && isDefined(details) && details !== '')
+    return true;
+
+  return false;
+}
+
+function dateObjectIsValid(dateObject) {
+  if (isDefined(dateObject) && dateObject != 'Invalid Date')
+    return true;
+
+  return false;
+}
+
+function getDateString(date) {
+  let dateObj = new Date(date);
+  if (!dateObjectIsValid(dateObj))
+    dateObj = new Date(Date.now());
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const dateStr = dateObj.toLocaleDateString(undefined, options);
+  return dateStr;
+}
+
+function postListReducerCallback(list, post) {
+  if (postIsValid(post)) {
+    const { date, header, details } = post;
+    const dateStr = getDateString(date);
+
+    list.push({
+      date: dateStr,
+      header: header,
+      details: details
+    });
+  }
+
+  return list;
+}
+
+function reducePostList(postList) {
+  return postList.reduce((list, post) => postListReducerCallback(list, post), []);
 }
 
 function sortPostList(postList) {
   let posts = postList;
-  posts.forEach(post => {
-    const { month, year } = post;
-    post.order = Date.parse(`01 ${month} ${year}`);
-  });
+  // TODO: check if this works.
+  posts.forEach(post => post.order = Date.parse(post.date));
+  // posts.forEach(post => {
+  //   post.order = Date.parse(post.date);
+  // });
 
-  return sortListByOrder(posts);
+  posts = sortListByReverseOrder(posts);
+  return posts;
 }
 
-function buildPostList(list) {
-  // if (!isDefined(list) || list.length === 0)
-  //   return [];
+function buildPostListJsx(id, list) {
+  if (!isDefined(list) || list.length === 0)
+    return;
 
-  // let postList = reduceSlideList(list);
-  // postList = sortPostList(postList);
+  let postList = reducePostList(list);
+  postList = sortPostList(postList);
 
-  // return postList;
-  return list
+  return (
+    postList.map((post, i) => {
+      const key = `${id}Post-${i}`;
+      const className = i % 2 === 0 ? styles.leftPost : styles.rightPost;
+      const { date, header, details } = post;
+
+      return (
+        <li key={key} className={className} >
+          <div className={styles.postHeader}>
+            <h1>{header}</h1>
+            <p>{date}</p>
+          </div>
+          <div className={styles.postDetails}>
+            <p>{details}</p>
+          </div>
+        </li>
+      );
+    })
+  );
 }
 
 export default function Announcements({ announcements }) {
   const id = isDefined(announcements.name) ? announcements.name : 'announcements';
-  const postList = buildPostList(announcements.postList);
+  const postListJsx = buildPostListJsx(id, announcements.postList);
 
   return (
     <ul id={`${id}Announcements`} className={styles.announcements}>
-      {
-        postList.map((post, i) => {
-          const key = `${announcements.name}Post-${i}`;
-          const className = i % 2 === 0 ? styles.leftPost : styles.rightPost;
-          const { date, header, details } = post;
-          let dateStr = new Date(date).toLocaleDateString();
-          if (dateStr === 'Invalid Date' || dateStr === '1970-01-01T00:00:00.000Z') dateStr = '';
-
-          return (
-            <li key={key} className={className} >
-              <div className={styles.postHeader}>
-                <h1>{header}</h1>
-                <p>{dateStr}</p>
-              </div>
-              <div className={styles.postDetails}>
-                <p>{details}</p>
-              </div>
-            </li>
-          );
-        })
-      }
+      {postListJsx}
     </ul>
   );
 }

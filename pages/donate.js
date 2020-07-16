8bbeb49c98c4b9039c0503438262037f9397d39a
elp49 <elp49@drexel.edu>
2020-07-16 18:20:40 -0400

    Donate - adding Donate page for users to make donations, installing stripe packages and axios to process payments in the backend

---
+++ pages/donate.js
@@ -0,0 +1,65 @@
+import donate from './donate.json'
+import Layout from '../components/Layout'
+
+const PAGE_NAME = 'donate';
+
+function buildDonateSection(donate) {
+  if (!isDefined(donate))
+    return;
+
+  const donateJsx = (
+    <Section sectionDetails={donate.sectionDetails}>
+      <p>This is from the donation section</p>
+    </Section>
+  );
+
+  return {
+    order: donate.order,
+    name: donate.sectionDetails.name,
+    jsx: donateJsx
+  };
+}
+
+function buildSections(pageSections) {
+  const donateSection = buildDonateSection(pageSections.donate);
+
+  return {
+    donate: donateSection,
+  };
+}
+
+function buildSectionList(pageSections) {
+  if (!isDefined(pageSections))
+    return;
+
+  const sections = buildSections(pageSections);
+  let sectionList = [];
+  for (const s in sections)
+    if (isDefined(sections[s]))
+      sectionList.push(sections[s]);
+
+  sectionList = sortListByOrder(sectionList);
+
+  return sectionList;
+}
+
+export default function Donate() {
+  const sectionList = buildSectionList(donate.sections);
+
+  return (
+    <Layout>
+      {
+        sectionList.map(section => {
+          const key = `${PAGE_NAME}Section-${section.order}`;
+          const id = `${section.name}`;
+
+          return (
+            <div key={key} id={id}>
+              {section.jsx}
+            </div>
+          );
+        })
+      }
+    </Layout>
+  );
+}
